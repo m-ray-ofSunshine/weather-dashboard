35feb2f61450c195d391ed552b87e4b5b36b8fb0
mray1995 <sleepingbag5927@gmail.com>
2021-06-23 10:02:45 -0500

    Testing iteration for 5 day forecast.

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,5 +39,8 @@ function citySearch(e) {
          })
          .then(function(data) {
              console.log(data);
+             for(var i=0; i>data.list.length; i++){
+             console.log(data.list[i]);
+             }
          })
  }
