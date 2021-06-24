1e9734e7930d1a627df17d7312f6fcd531b1d492
mray1995 <sleepingbag5927@gmail.com>
2021-06-23 20:38:47 -0500

    Added JS to dynamically add the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,20 @@ var apiKey = "fd1f2ef3b4987995a3d25c966c0dded0"
 
 $("form").submit(citySearch)
 
+function getCityDate(unix) {
+    var a = new Date(unix * 1000);
+    var cityDate = " ("+(a.getMonth()+1)+"-"+a.getDate()+"-"+a.getFullYear()+") ";
+    return cityDate;
+}
+
+function getIconImage (iconCode) {
+    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    var image = $("<img>")
+    image.attr("src", iconUrl);
+    return image;
+}
+
+
 function citySearch(e) {
     e.preventDefault();
     var city =$("#city").val();
@@ -14,23 +28,20 @@ function citySearch(e) {
          })
          .then(function(data) {
              var cityName = $("#cityName")
-             //var temperature = $("<p>");
+             
              var temperature = $("<p>").text("Temp: " + data.main.temp + " F\u00B0")
              var windSpeed = $("<p>").text("Wind Speed: " + data.wind.speed + "mph")
              var humidity = $("<p>").text("Humidity: " + data.main.humidity + "%")
              var currentWeather = $("#currentWeather")
-             var iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-             var image = $("<img>")
-             image.attr("src", iconUrl);
-             var a = new Date(data.dt * 1000);
-             var cityDate = " ("+(a.getMonth()+1)+"-"+a.getDate()+"-"+a.getFullYear()+") ";
+             var image = getIconImage(data.weather[0].icon)
+             var cityDate = getCityDate(data.dt)
              cityName[0].innerHTML= data.name + cityDate 
              cityName.append(image)
              currentWeather.append(cityName)
              currentWeather.append(temperature)
              currentWeather.append(windSpeed)
              currentWeather.append(humidity)
-             //console.log(data)
+             console.log(data)
 
          })
          fetch (apiUrl5DayForcast)
@@ -38,9 +49,25 @@ function citySearch(e) {
              return response.json();
          })
          .then(function(data) {
-             console.log(data);
-             for(var i=0; i>data.list.length; i++){
-             console.log(data.list[i]);
-             }
+             var fiveDayCard = $("#fiveDayCard")
+             var fiveDayTitle = $("#fiveDayTitle").text("5-Day Forecast")
+             fiveDayCard.append(fiveDayTitle);
+             for(var i=0; i < data.list.length; i+=8){
+                 
+                 var container = $("#fiveDay");
+                 var day = $("<div>").attr("class", "col-2");
+                 var date = $("<h4>").text(getCityDate(data.list[i].dt));
+                 var image = getIconImage(data.list[i].weather[0].icon)
+                 var temp = $("<p>").text("Temp: " + data.list[i].main.temp + " F\u00B0");
+                 var wind =$("<p>").text("Wind: " + data.list[i].wind.speed + "mph")
+                 var humidity = $("<p>").text("Humidity: " + data.list[i].main.humidity + "%")
+                 day.append(date);
+                 day.append(image);
+                 day.append(temp);
+                 day.append(wind);
+                 day.append(humidity);
+                 container.append(day);
+                }
+                console.log(data);
          })
  }
