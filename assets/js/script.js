1f8fec593ee23c1423b38ec7ac200b06b75c62ad
mray1995 <sleepingbag5927@gmail.com>
2021-06-25 23:38:40 -0500

    Final draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var previousSearches = [];
 function citySearch(e) {
     e.preventDefault();
     city = $("#city").val()
+   if(city){
     $("#currentWeatherCard").empty()
     $("#fiveDayCard").empty()
 
@@ -12,6 +13,7 @@ function citySearch(e) {
     get5DayForecast(city)
     
     $("#city").val("")
+   }
     
 }
 function redoSearch(city) {
@@ -46,7 +48,7 @@ function renderCurrentWeather(currentData){
         var currentWeather = $("#currentWeatherCard")
         var image = getIconImage(currentData.weather[0].icon)
         var cityDate = getCityDate(currentData.dt)
-        cityName[0].innerHTML = currentData.name + cityDate
+        cityName[0].innerHTML = "Current Conditions: " + currentData.name + cityDate
         cityName.append(image)
         currentWeather.append(cityName)
         currentWeather.append(temperature)
@@ -69,18 +71,18 @@ function renderCurrentWeather(currentData){
     }
 
     function render5DayForecast (currentForecast){
-        var fiveDayCard = $("#fiveDayCard")
-            var fiveDayTitle = $("<h3>").text("5-Day Forecast")
+            var fiveDayCard = $("#fiveDayCard");
+            var fiveDayTitle = $("<h3>").text("5-Day Forecast");
             fiveDayCard.append(fiveDayTitle);
-            var container = $("<div>").attr("class", "row");
+            var container = $("<div>").addClass( "row"  );
             fiveDayCard.append(container)
             
             for (var i = 0; i < currentForecast.list.length; i += 8) {
                 
-                var day = $("<div>").attr("class", "col-2");
+                var day = $("<div>").addClass( "col-sm-12 col-md-4 col-lg-2 test2 d-flex flex-column justify-content-center fiveDayCard" );
                 var date = $("<h4>").text(getCityDate(currentForecast.list[i].dt));
                 var image = getIconImage(currentForecast.list[i].weather[0].icon)
-                var temp = $("<p>").text("Temp: " + currentForecast.list[i].main.temp + " F\u00B0");
+               var temp = $("<p>").text("Temp: " + currentForecast.list[i].main.temp + " F\u00B0");
                 var wind = $("<p>").text("Wind: " + currentForecast.list[i].wind.speed + "mph")
                 var humidity = $("<p>").text("Humidity: " + currentForecast.list[i].main.humidity + "%")
                 day.append(date);
@@ -102,7 +104,7 @@ function getCityDate(unix) {
 function getIconImage(iconCode) {
     var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     var image = $("<img>")
-    image.attr("src", iconUrl);
+    image.attr({src: iconUrl, width: 50, height: 50});
     return image;
 }
 
